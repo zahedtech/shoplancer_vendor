e5f33c0f16d58539eca24185cd8724d9ed0b2cc2
Hashem Naim <hashemnaim3@gmail.com>
2026-08-06 06:08:03 +0300

    fix - upload

--- test_icon.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-void main() {
-  print('Icons.arrow_back_ios: ${Icons.arrow_back_ios.matchTextDirection}');
-  print('Icons.close: ${Icons.close.matchTextDirection}');
-  print('Icons.delete_forever: ${Icons.delete_forever.matchTextDirection}');
-  print('Icons.copy: ${Icons.copy.matchTextDirection}');
-  print('Icons.calendar_month: ${Icons.calendar_month.matchTextDirection}');
-}
